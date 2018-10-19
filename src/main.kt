8c622d729ace45e6b31c33a5dee78c6869d32f7a
Aaron Alef <aaron.alef@code.berlin>
2018-10-19 23:46:36 +0200

    Bug fix: Win detection only makes sense for diagonal lines if on middle line... Code now ugly again

--- src/main.kt
+++ src/main.kt
@@ -3,14 +3,13 @@ import java.lang.Math.*
 const val MIN_RANGE = 1     // Must stay positive; limitting size of game with 1 being the smallest
 const val MAX_RANGE = 20    // Maximum field size; up to 32767 (though not recommended)
 
+var fieldSize: Int
+
 fun main(args: Array<String>)
 {
     // Get valid field size + create field (2D-String-Array)
     var fieldSize = getInt("Input Field Size: ", MIN_RANGE..MAX_RANGE)!!
     val field = array2d<String?>(fieldSize, fieldSize) { "#" }
-
-    // Print the field to ensure it's correctly created
-    printField(field)
 
     // Start the game and print the winner
     println("The winner is: " + play(field))
@@ -21,7 +20,6 @@ fun main(args: Array<String>)
 // TODO: Changing 'if' construct to 'when' construct for aesthetic reasons
 fun printField(field: Array<Array<String?>>)
 {
-    var fieldSize = field.size
     for (i in 0..(fieldSize * 2))
     {
         for (j in 0..((fieldSize + 1) * 3 + fieldSize - 2))
@@ -62,7 +60,6 @@ fun getInt(msg: String, range: IntRange): Int?
 //Method to start a new "play" instance
 fun play(field: Array<Array<String?>>): String
 {
-    var fieldSize = field.size
     var symbols = arrayOf ("X", "O")
     var player = 0
     var x: Int
@@ -70,7 +67,8 @@ fun play(field: Array<Array<String?>>): String
 
     for (turn in 0..pow(fieldSize.toDouble(), 2.0).toInt())
     {
-        // Setup Phase
+        // Setup Phase: Print map and current player
+        printField(field)
         println("Player " + (player + 1))
 
         // Playing Phase: Getting coordinates of field
@@ -90,21 +88,20 @@ fun play(field: Array<Array<String?>>): String
 // Checks for a win; X/O if successful, # otherwise
 fun check(field: Array<Array<String?>>, player: String, x: Int, y: Int): Boolean
 {
-    for (dx in 0..1)
-    {
-        for (dy in 0..1)
-        {
-            // Starting recursive search for each of the four dimensions
-            if (fieldExtension(field,  player, x, y, 2 * dx - 1, dy) && fieldExtension(field,  player, x, y, -2 * dx + 1, -dy)) return true
-        }
-    }
+    // Starting recursive search; in either kardinal direction, and, if on the middle diagonal line, in those
+    if (fieldExtension(field,  player, x, y, ran, ran - 1) &&
+            fieldExtension(field,  player, x, y, -ran, -ran + 1)) return true
+    if ((fieldSize % 2 == 1) && abs(x) == abs(y)) return ((fieldExtension(field,  player, x, y, 1, 1) &&
+                                                            fieldExtension(field,  player, x, y, -1, -1)) ||
+                                                            (fieldExtension(field,  player, x, y, 1, -1) &&
+                                                            fieldExtension(field,  player, x, y, -1, 1)))
     return false
 }
 
 // Checks recursively every lane from the x-y-coordinate
 fun fieldExtension(field: Array<Array<String?>>, player: String, x: Int, y: Int, dx: Int, dy: Int): Boolean
 {
-    if ((x in MIN_RANGE..MAX_RANGE) && (y in MIN_RANGE..MAX_RANGE))
+    if ((x in 0 until fieldSize - 1) && (y in 0 until fieldSize - 1))
     {
         if (player != field[x][y]) return false
         else return fieldExtension(field, player,x + dx, y + dy, dx, dy)
