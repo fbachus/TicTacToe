0e0f0fc60387def620a3d53684844ed7082ba069
Aaron Alef <aaron.alef@code.berlin>
2018-10-19 15:50:12 +0200

    finished PrettyPrinting as console output. TODO: Implement an actual interface; fix input validation

--- src/main.kt
+++ src/main.kt
@@ -1,64 +1,89 @@
 import java.util.*
+import java.lang.Math.*
 
-
-var field = Array(9) {_ -> "#"}
 var symbols = arrayOf ("X", "O")
+var fieldSize = 3
 
-fun main(args: Array<String>) {
-    println("The winner is: " + play())
-    printField()
+fun main(args: Array<String>)
+{
+    // Get valid field size
+    do {
+        fieldSize = getInt("Input Field Size: ")!!
+    } while (fieldSize <= 0)
+    val field = array2d<String?>(fieldSize, fieldSize) { "#" }
+
+    // print the field to ensure it's correctly created
+    printField(field)
+
+
+    //println("The winner is: " + play())
 }
 
-fun printField(){
-    for (i in 0..8)
+fun printField(field: Array<Array<String?>>)
+{
+    for (i in 0..((fieldSize + 1) * 3 + fieldSize - 2))
     {
-        print(field[i])
-        if (i % 3 == 2) print("\n")
+        for (j in 0..((fieldSize + 1) * 3 + fieldSize - 2))
+        {
+            if (i == 0)
+            {
+                if (j % 4 == 0) print(ceil((j / 4).toDouble()).toInt())
+                else if (j % 4 == 2) print("|")
+                else print(" ")
+            }
+            else if (i % 2 == 1)
+            {
+                if (j % 4 == 2) print("|")
+                else print(" ")
+            }
+            else if (i % 4 == 2) print("-")
+            else
+            {
+                if (j == 0) print(ceil((i / 4).toDouble()).toInt())
+                else if (j % 2 == 1) print(" ")
+                else if (j % 4 == 2) print("|")
+                else
+                {
+                    print(field[ceil((i / 4).toDouble()).toInt() - 1][ceil((j / 4).toDouble()).toInt() - 1])
+                }
+            }
+        }
+        print("\n")
     }
+}
+
+//TODO: FIX THE GODDAMN INPUT VALIDATION
+fun getInt(msg: String): Int?
+{
+    print(msg)
+    return readLine()?.toInt()
 }
 
 fun play(): String
 {
     var player = 0
-    var index = 0
-    for (i in 0..8){
-        printField()
-        println("player: " + symbols[player] + "\n")
-        do {
-            val input = Scanner(System.`in`)
-            index = fieldcon(input.nextInt())
-        } while (index < 0 || index > 8 || field[index] != "#")
-        println("Field:  " + index.toString())
-        field[index] = symbols[player]
-        var checked = check(player, index)
-        if (checked == "X" || checked == "O") return checked
-        player = (player + 1) % 2
-    }
+    var x: Int
+    var y: Int
+
+    
     return "Noone!"
 }
 
-fun fieldcon(index: Int): Int
-{
-    return when (index)
-    {
-        1 -> 6
-        2 -> 7
-        3 -> 8
-        4 -> 3
-        5 -> 4
-        6 -> 5
-        7 -> 0
-        8 -> 1
-        9 -> 2
-        else -> 42
-    }
-}
+//fun check(player: Int, x: Int, y: Int): String
+//{
 
-fun check(player: Int, index: Int): String
-{
-    if ((field[index % 3] == symbols[player] && field[index % 3 + 3] == symbols[player] && field[index % 3 + 6] == symbols[player]) ||
-            (field[index % 3] == symbols[player] && field[index % 3 + 1] == symbols[player] && field[index % 3 + 2] == symbols[player]) ||
-            (index % 2 == 0 && ((field[0] == symbols[player] && field[4] == symbols[player] && field[8] == symbols[player]) ||
-            (field[2] == symbols[player] && field[4] == symbols[player] && field[6] == symbols[player])))) return symbols[player]
-    else return "#"
-}
+//}
+
+// Functions to create a 2D Array (copied straight from StackOverflow): https://stackoverflow.com/questions/27512636/two-dimensional-int-array-in-kotlin
+public inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int)->INNER): Array<Array<INNER>>
+        = Array(sizeOuter) { Array<INNER>(sizeInner, innerInit) }
+public fun array2dOfInt(sizeOuter: Int, sizeInner: Int): Array<IntArray>
+        = Array(sizeOuter) { IntArray(sizeInner) }
+public fun array2dOfLong(sizeOuter: Int, sizeInner: Int): Array<LongArray>
+        = Array(sizeOuter) { LongArray(sizeInner) }
+public fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray>
+        = Array(sizeOuter) { ByteArray(sizeInner) }
+public fun array2dOfChar(sizeOuter: Int, sizeInner: Int): Array<CharArray>
+        = Array(sizeOuter) { CharArray(sizeInner) }
+public fun array2dOfBoolean(sizeOuter: Int, sizeInner: Int): Array<BooleanArray>
+        = Array(sizeOuter) { BooleanArray(sizeInner) }
