68324788d95899ee1583db7742dd4437226eec7b
Aaron Alef <aaron.alef@code.berlin>
2018-10-19 23:19:09 +0200

    Uncommenting of lines, improving readability of code, making cosmethic changes

--- src/main.kt
+++ src/main.kt
@@ -1,28 +1,27 @@
-import java.util.*
 import java.lang.Math.*
 
-var fieldSize = 3
-
-const val MAX_RANGE = 20
-const val MIN_RANGE = 1
+const val MIN_RANGE = 1     // Must stay positive; limitting size of game with 1 being the smallest
+const val MAX_RANGE = 20    // Maximum field size; up to 32767 (though not recommended)
 
 fun main(args: Array<String>)
 {
-    // Get valid field size
-    fieldSize = getInt("Input Field Size: ", MIN_RANGE..MAX_RANGE)!!
+    // Get valid field size + create field (2D-String-Array)
+    var fieldSize = getInt("Input Field Size: ", MIN_RANGE..MAX_RANGE)!!
     val field = array2d<String?>(fieldSize, fieldSize) { "#" }
 
-    // print the field to ensure it's correctly created
+    // Print the field to ensure it's correctly created
     printField(field)
 
     // Start the game and print the winner
-    //println("The winner is: " + play())
+    println("The winner is: " + play(field))
 }
 
 // Pretty-Printing the field
-//TODO: Implement actual interactive interface
+// TODO: Implement actual interactive interface
+// TODO: Changing 'if' construct to 'when' construct for aesthetic reasons
 fun printField(field: Array<Array<String?>>)
 {
+    var fieldSize = field.size
     for (i in 0..(fieldSize * 2))
     {
         for (j in 0..((fieldSize + 1) * 3 + fieldSize - 2))
@@ -49,7 +48,7 @@ fun printField(field: Array<Array<String?>>)
     }
 }
 
-//TODO: FIX THE GODDAMN INPUT VALIDATION
+// TODO: Fix input validation; >> currently not working
 fun getInt(msg: String, range: IntRange): Int?
 {
     var tmp: Int
@@ -57,11 +56,13 @@ fun getInt(msg: String, range: IntRange): Int?
     do {
         tmp = readLine()?.toInt()!!
     } while (!(tmp in range))
+    return tmp
 }
 
 //Method to start a new "play" instance
 fun play(field: Array<Array<String?>>): String
 {
+    var fieldSize = field.size
     var symbols = arrayOf ("X", "O")
     var player = 0
     var x: Int
@@ -69,8 +70,10 @@ fun play(field: Array<Array<String?>>): String
 
     for (turn in 0..pow(fieldSize.toDouble(), 2.0).toInt())
     {
-        // Starting Phase: Resetting vars, getting new field from new player
+        // Setup Phase
         println("Player " + (player + 1))
+
+        // Playing Phase: Getting coordinates of field
         x = getInt("Row:    ", MIN_RANGE..MAX_RANGE)!!
         y = getInt("Column: ", MIN_RANGE..MAX_RANGE)!!
 
@@ -87,37 +90,29 @@ fun play(field: Array<Array<String?>>): String
 // Checks for a win; X/O if successful, # otherwise
 fun check(field: Array<Array<String?>>, player: String, x: Int, y: Int): Boolean
 {
-    for (dx in -1..1)
+    for (dx in 0..1)
     {
-        for (dy in -1..1)
+        for (dy in 0..1)
         {
-            if (fieldExtension(field,  player, x, y, dx, dy)) return true
+            // Starting recursive search for each of the four dimensions
+            if (fieldExtension(field,  player, x, y, 2 * dx - 1, dy) && fieldExtension(field,  player, x, y, -2 * dx + 1, -dy)) return true
         }
     }
     return false
 }
 
-// checks recursively every lane from the x-y-coordinate
+// Checks recursively every lane from the x-y-coordinate
 fun fieldExtension(field: Array<Array<String?>>, player: String, x: Int, y: Int, dx: Int, dy: Int): Boolean
 {
     if ((x in MIN_RANGE..MAX_RANGE) && (y in MIN_RANGE..MAX_RANGE))
     {
         if (player != field[x][y]) return false
-        else return adjFields(field, player,x + dx, y + dy, dx, dy)
+        else return fieldExtension(field, player,x + dx, y + dy, dx, dy)
     }
     else return true
 }
 
-// Functions to create a 2D Array (copied straight from StackOverflow): https://stackoverflow.com/questions/27512636/two-dimensional-int-array-in-kotlin
-public inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int)->INNER): Array<Array<INNER>>
+// Function to create a 2D Array (copied from StackOverflow and removed redundant modifiers)
+// Source: https://stackoverflow.com/questions/27512636/two-dimensional-int-array-in-kotlin
+inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int)->INNER): Array<Array<INNER>>
         = Array(sizeOuter) { Array<INNER>(sizeInner, innerInit) }
-public fun array2dOfInt(sizeOuter: Int, sizeInner: Int): Array<IntArray>
-        = Array(sizeOuter) { IntArray(sizeInner) }
-public fun array2dOfLong(sizeOuter: Int, sizeInner: Int): Array<LongArray>
-        = Array(sizeOuter) { LongArray(sizeInner) }
-public fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray>
-        = Array(sizeOuter) { ByteArray(sizeInner) }
-public fun array2dOfChar(sizeOuter: Int, sizeInner: Int): Array<CharArray>
-        = Array(sizeOuter) { CharArray(sizeInner) }
-public fun array2dOfBoolean(sizeOuter: Int, sizeInner: Int): Array<BooleanArray>
-        = Array(sizeOuter) { BooleanArray(sizeInner) }
